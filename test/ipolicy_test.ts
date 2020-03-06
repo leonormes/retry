4c831a475d851b8d65cf11dafd7b7cd280f99290
Leon Ormes <leon.ormes@tessian.com>
2020-03-06 12:23:41 +0000

    update ipolicy

--- test/ipolicy_test.ts
+++ test/ipolicy_test.ts
@@ -5,21 +5,23 @@ describe('constantPolicy', function() {
     it('should be a poicy', function() {
         const pol = new ConstantPolicy();
         const pol0 = new ConstantPolicy(0);
-        const pol1 = new ConstantPolicy(1);
+		const pol1 = new ConstantPolicy(1);
+
         assert.equal(pol.currentWait(), 500);
         assert.equal(pol.maxTime, 2500);
         assert.ok(pol.shouldRetry());
-        // t.false(pol0.shouldRetry())
+        assert.equal(pol0.shouldRetry(), false)
         assert.ok(pol1.shouldRetry());
         pol1.incrementTry();
-        // t.false(pol1.shouldRetry())
+        assert.equal(pol1.shouldRetry(), false)
     });
 });
 describe('expoPolicy', function() {
     it('should backoff', function() {
         const pol = new ExpoPolicy();
         const pol0 = new ExpoPolicy(0);
-        const pol1 = new ExpoPolicy(1);
+		const pol1 = new ExpoPolicy(1);
+
         assert.equal(pol.currentWait(), 1);
         assert.equal(pol.maxTime, 2500);
         assert.ok(pol.shouldRetry());
