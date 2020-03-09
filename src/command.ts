a58e6290b67fb49161a342ebbd9d871f942d60a9
Leon Ormes <leon.ormes@tessian.com>
2020-03-09 09:32:33 +0000

    format

--- src/command.ts
+++ src/command.ts
@@ -3,10 +3,9 @@ export interface ICommand {
 }
 
 export class Command<T, U> implements ICommand {
-           constructor(private fn: (payload: T | undefined) => Promise<U>, private payload?: T) {
-           }
+    constructor(private fn: (payload: T | undefined) => Promise<U>, private payload?: T) {}
 
-           public async execute(): Promise<U> {
-               return await this.fn(this.payload);
-           }
-       }
+    public async execute(): Promise<U> {
+        return await this.fn(this.payload);
+    }
+}
